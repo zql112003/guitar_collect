babf4693e29f11b3b51433686010a8619aebf168
zql112003 <247882033@qq.com>
2016-12-19 01:50:40 -0800

    beautifulSoul4

---
+++ getYahooJp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+#encoding:UTF-8
+import urllib.request
+from bs4 import BeautifulSoup
+
+i = 0
+while i < 20:
+
+	url = "http://auctions.search.yahoo.co.jp/search?p=ibanez&oq=&n=100&auccat=0&tab_ex=commerce&ei=UTF-8&b=101"
+	print(url)
+	data = urllib.request.urlopen(url).read()
+	data = data.decode('UTF-8')
+
+	soup = BeautifulSoup(data,'html5lib')
+	body = soup.body
+
+	#从文档中找到所有<a>标签的内容  
+	for link in body.find_all('h3'):  
+	    print(link.a.text)
+
+#print(soup.head)
